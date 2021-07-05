74535b84683a7ed13b316bbe96501e22f187e460
lmwk <bladedshogun@gmail.com>
2021-07-05 18:11:31 -0500

    - Made a skybox but... it's scuffed
    - A little airplane to go along with it :)

--- src/main.cpp
+++ src/main.cpp
@@ -34,6 +34,35 @@ float rectangleVertices[] =
     -1.0f,  1.0f,  0.0f, 1.0f,
 };
 
+float skyboxVertices[] =
+{
+    //   Coordinates
+    -1.0f, -1.0f,  1.0f,
+     1.0f, -1.0f,  1.0f,
+     1.0f, -1.0f, -1.0f,
+    -1.0f, -1.0f, -1.0f,
+    -1.0f,  1.0f,  1.0f,
+     1.0f,  1.0f,  1.0f,
+     1.0f,  1.0f, -1.0f,
+    -1.0f,  1.0f, -1.0f
+};
+
+unsigned int skyboxIndices[] =
+{
+    1, 2, 6,
+    6, 5, 1,
+    0, 4, 7,
+    7, 3, 0,
+    4, 5, 6,
+    6, 7, 4,
+    0, 3, 2,
+    2, 1, 0,
+    0, 1, 5,
+    5, 4, 0,
+    3, 7, 6,
+    6, 2, 3
+};
+
 //Function that makes everything go brrrrrrrrrrrrrrrrrrrrrrrrr
 int main()
 {
@@ -66,9 +95,8 @@ int main()
     //Gary draws things
 
     Shader shaderProgram("./Resources/shaders/defualt.vert", "./Resources/shaders/defualt.frag");
-    Shader grassProgram("./Resources/shaders/defualt.vert", "./Resources/shaders/grass.frag");
-    Shader winProgram("./Resources/shaders/defualt.vert", "./Resources/shaders/windows.frag");
     Shader frameProgram("./Resources/shaders/framebuffer.vert", "./Resources/shaders/framebuffer.frag");
+    Shader skyShader("./Resources/shaders/skybox.vert", "./Resources/shaders/skybox.frag");
 
 
     glm::vec4 lightColor = glm::vec4(1.0f, 1.0f, 1.0f, 1.0f);
@@ -80,11 +108,10 @@ int main()
     shaderProgram.Activate();
     glUniform4f(glGetUniformLocation(shaderProgram.ID, "lightColor"), lightColor.x, lightColor.y, lightColor.z, lightColor.w);
     glUniform3f(glGetUniformLocation(shaderProgram.ID, "lightPos"), lightPos.x, lightPos.y, lightPos.z);
-    grassProgram.Activate();
-    glUniform4f(glGetUniformLocation(grassProgram.ID, "lightColor"), lightColor.x, lightColor.y, lightColor.z, lightColor.w);
-    glUniform3f(glGetUniformLocation(grassProgram.ID, "lightPos"), lightPos.x, lightPos.y, lightPos.z);
     frameProgram.Activate();
     glUniform1i(glGetUniformLocation(frameProgram.ID, "screenTexture"), 0);
+    skyShader.Activate();
+    glUniform1i(glGetUniformLocation(skyShader.ID, "skybox"), 0);
 
     glEnable(GL_DEPTH_TEST);
 
@@ -97,10 +124,8 @@ int main()
     Camera camera(width, height, glm::vec3(0.0f, 0.0f, 2.0f));
 
     prevTime = glfwGetTime();
-    
-    ZModel ground("./Resources/models/ground/scene.gltf");
-    ZModel grass("./Resources/models/grass/scene.gltf");
-    ZModel windows("./Resources/models/windows/scene.gltf");
+
+
 
     //glfwSwapInterval(0);
 
@@ -153,6 +178,70 @@ int main()
     if (fboStatus != GL_FRAMEBUFFER_COMPLETE)
         std::cout << "Framebuffer error: " << fboStatus << std::endl;
 
+    unsigned int skyboxVAO, skyboxVBO, skyboxEBO;
+    glGenVertexArrays(1, &skyboxVAO);
+    glGenBuffers(1, &skyboxVBO);
+    glGenBuffers(1, &skyboxEBO);
+    glBindVertexArray(skyboxVAO);
+    glBindBuffer(GL_ARRAY_BUFFER, skyboxVBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(skyboxVertices), &skyboxVertices, GL_STATIC_DRAW);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, skyboxEBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(skyboxIndices), &skyboxIndices, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+    std::string facesCubemap[6] =
+    {
+        "./Resources/TestTextures/skybox/right.jpg",
+        "./Resources/TestTextures/skybox/left.jpg",
+        "./Resources/TestTextures/skybox/top.jpg",
+        "./Resources/TestTextures/skybox/bottom.jpg",
+        "./Resources/TestTextures/skybox/front.jpg",
+        "./Resources/TestTextures/skybox/back.jpg",
+    };
+
+    unsigned int cubemapTexture;
+    glGenTextures(1, &cubemapTexture);
+    glBindTexture(GL_TEXTURE_CUBE_MAP, cubemapTexture);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
+
+    for (unsigned int i = 0; i < 6; i++)
+    {
+        int width, height, nrChannels;
+        unsigned char* data = stbi_load(facesCubemap[i].c_str(), &width, &height, &nrChannels, 0);
+        if (data)
+        {
+            stbi_set_flip_vertically_on_load(false);
+            glTexImage2D
+            (
+                GL_TEXTURE_CUBE_MAP_POSITIVE_X + i,
+                0,
+                GL_RGB,
+                width,
+                height,
+                0,
+                GL_RGB,
+                GL_UNSIGNED_BYTE,
+                data
+            );
+            stbi_image_free(data);
+        }
+        else
+        {
+            std::cout << "Failed to load texture: " << facesCubemap[i] << std::endl;
+            stbi_image_free(data);
+        }
+    }
+
+    ZModel model("./Resources/models/airplane/scene.gltf");
+
     //Making gary into an off-brand smurf
     //A loop if you couldn't tell, don't worry I got you
     while (!glfwWindowShouldClose(window))
@@ -183,21 +272,26 @@ int main()
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         glEnable(GL_DEPTH_TEST);
 
-        ground.Draw(shaderProgram, camera);
-        glDisable(GL_CULL_FACE);
-        grass.Draw(grassProgram, camera);
-        glEnable(GL_BLEND);
-        for (unsigned int i = 0; i < numWins; i++)
-        {
-            distanceCamera[i] = glm::length(camera.Position - positionsWin[i]);
-        }
-        qsort(orderDraw, numWins, sizeof(unsigned int), compare);
-        for (unsigned int i = 0; i < numWins; i++)
-        {
-            windows.Draw(winProgram, camera, positionsWin[orderDraw[i]], glm::quat(1.0f, 0.0f, rotationsWin[orderDraw[i]], 0.0f));
-        }
-        glDisable(GL_BLEND);
+        model.Draw(shaderProgram, camera);
 
+        glDepthFunc(GL_LEQUAL);
+        skyShader.Activate();
+        glm::mat4 view = glm::mat4(1.0f);
+        glm::mat4 projection = glm::mat4(1.0f);
+        view = glm::mat4(glm::mat3(glm::lookAt(camera.Position, camera.Position + camera.Orientation, camera.Up)));
+        projection = glm::perspective(glm::radians(45.0f), (float)width / height, 0.1f, 100.0f);
+        glUniformMatrix4fv(glGetUniformLocation(skyShader.ID, "view"), 1, GL_FALSE, glm::value_ptr(view));
+        glUniformMatrix4fv(glGetUniformLocation(skyShader.ID, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
+
+        glBindVertexArray(skyboxVAO);
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_CUBE_MAP, cubemapTexture);
+        glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
+        glBindVertexArray(0);
+
+        glDepthFunc(GL_LESS);
+
+        glDisable(GL_CULL_FACE);
         glBindFramebuffer(GL_FRAMEBUFFER, 0);
         frameProgram.Activate();
         glBindVertexArray(rectVAO);
@@ -212,7 +306,8 @@ int main()
     }
     //Deleting shaders
     shaderProgram.Delete();
-    grassProgram.Delete();
+    frameProgram.Delete();
+    skyShader.Delete();
     //Killing garry :(
     glfwDestroyWindow(window);
     glfwTerminate();
