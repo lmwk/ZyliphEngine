6728e7837569b0a7e3ffd55bce3a5dc006fb57a9
lmwk <bladedshogun@gmail.com>
2021-07-15 17:09:46 -0500

    Trying to create an ECS

--- src/HeaderFiles/ZScene.h
+++ src/HeaderFiles/ZScene.h
@@ -6,16 +6,15 @@
 #include "../HeaderFiles/Camera.h"
 #include "../HeaderFiles/shaderClass.h"
 #include "../HeaderFiles/Light.h"
-#include "../HeaderFiles/ZObject.h"
+#include "../HeaderFiles/ECS.h"
 
 class ZScene
 {
 public:
-    ZScene() : zCamera(nullptr), zPostProcessing(nullptr), zBaseShader(nullptr), zLights(nullptr), zSize(800, 600)
+    ZScene() : zCamera(nullptr), zPostProcessing(nullptr), zBaseShader(nullptr), zSize(800, 600)
     {
         zPostProcessing = std::make_unique<ZPostProcessingBuffer>();
         zBaseShader = std::make_unique<Shader>("fill eventually", "fill eventually");
-        zLights = std::make_unique<std::map<std::string, ZLight>>();
         zCamera = std::make_unique<Camera>(zSize.x, zSize.y, glm::vec3(0.0f, 0.0f, 2.0f));
     }
 
@@ -24,22 +23,21 @@ public:
         zBaseShader->Delete();
     }
 
-    ZLight get_light(std::string name);
-
-    ZObject get_object(std::string name);
+    std::shared_ptr<Entity> get_entity(std::string name);
 
     void resize_sceneView(int width, int height);
 
     void render();
 
-    void add_object(std::string name, ZObject object);
-    
+    Entity create_entity(std::string name);
+
+    void reset_cam() { zCamera->Position = glm::vec3(0.0f, 0.0f, 2.0f); }
+
 private:
+    Manager zManager;
     std::unique_ptr<Camera> zCamera;
     std::unique_ptr<ZPostProcessingBuffer> zPostProcessing;
     std::unique_ptr<Shader> zBaseShader;
-    std::unique_ptr<std::map<std::string,ZLight>> zLights;
-    std::unique_ptr<std::map<std::string,ZObject>> zObjects;
     glm::vec2 zSize;
 };
 
